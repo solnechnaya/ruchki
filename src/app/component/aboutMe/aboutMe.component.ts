c9acd60424d05073f48f6d17e251752df18f8676
solnechnaya <lev_n@bk.ru>
2018-01-03 18:14:41 +0600

    добавлены компоненты

---
+++ src/app/component/aboutMe/aboutMe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'aboutMe',
+  templateUrl: './aboutMe.component.html',
+  styleUrls: ['./aboutMe.component.css']
+})
+export class AboutMeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
